0c1e6dd1ef224a9888d28a1908361fcc86db0d8b
pfwrocks <pfwrocks@gmail.com>
2023-10-19 16:49:41 -0400

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react';
+
 export const metadata: Metadata = {
   title: "LLM Demo",
   description: "LLM Demo",
@@ -13,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}<Analytics/></body>
     </html>
   );
 }
