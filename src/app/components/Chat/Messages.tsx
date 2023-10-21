17c6a55a0c4607f620ce90acdb8b3fa1f61a6f11
pfwrocks <pfwrocks@gmail.com>
2023-10-21 05:30:02 +0000

    now has image abilities and has info about other datatypes

--- src/app/components/Chat/Messages.tsx
+++ src/app/components/Chat/Messages.tsx
@@ -1,9 +1,16 @@
 import { Message } from "ai";
 import { useRef } from "react";
 import {RiRobot2Line, RiUserFill} from "react-icons/ri"
+import Image from 'next/image'
 
 export default function Messages({ messages }: { messages: Message[] }) {
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
+
+  const extractImageUrls = (text: string) => {
+    const regex = /https:\/\/(?:image\.tmdb\.org|www\.hungrypaprikas\.com|www\.skinnytaste\.com|www\.restlesschipotle\.com|www\.allrecipes\.com|www\.theclevercarrot\.com|platedcravings\.com)\/.*?\.jpg/g;
+    return text.match(regex);
+  };
+
   return (
     <div className="border-2 border-gray-600 p-6 rounded-lg overflow-y-scroll flex-grow flex flex-col bg-gray-700">
       {messages.map((msg, index) => (
@@ -16,9 +23,21 @@ export default function Messages({ messages }: { messages: Message[] }) {
           <div className="rounded-tl-lg bg-gray-800 p-2 border-r border-gray-600 flex items-center">
             {msg.role === "assistant" ? <RiRobot2Line /> : <RiUserFill />}
           </div>
+          <div className="m1-2">
           <div className="ml-2 flex items-center text-gray-200">
             {msg.content}
           </div>
+          {msg.role === "assistant" && 
+              <div className="mt-2 flex">
+                {extractImageUrls(msg.content)?.slice(0, 6).map((url, imgIndex) => (
+                  <div key={imgIndex} className="ml-2">
+                    <Image src={url} width={200} height={200} alt="Image from content"/>
+                  </div>
+                ))}
+              </div>
+          }
+          
+        </div>
         </div>
       ))}
       <div ref={messagesEndRef} />
